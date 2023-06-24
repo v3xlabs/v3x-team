db3ec6577c3a6cd1a82cb9bceb66a9cfea122a61
Luc <luc@lucemans.nl>
2023-06-24 04:58:43 +0000

    Introduce Contributors

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { list } from '../list';
+import { general_contributors, list } from '../list';
 
 export default function App() {
     return (
@@ -10,34 +10,61 @@ export default function App() {
                     recent contributors
                 </div>
 
-                <div>Team ✨</div>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                    {list.map((member, index) => (
-                        <div key={index} className="flex w-full">
-                            <div className="w-24 h-24 aspect-square bg-neutral-300 relative overflow-hidden">
+                <div className="mt-4">
+                    <div>Team ✨</div>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
+                        {list.map((member, index) => (
+                            <div key={index} className="flex w-full">
+                                <div className="w-24 h-24 aspect-square bg-neutral-300 relative overflow-hidden">
+                                    {member.avatar && (
+                                        <img
+                                            src={member.avatar}
+                                            alt=""
+                                            className="w-full h-full object-cover grayscale"
+                                        />
+                                    )}
+                                    {!member.avatar && (
+                                        <div className="text-6xl grayscale left-1/2 -translate-x-1/2 absolute bottom-0">
+                                            🤷‍♀️
+                                        </div>
+                                    )}
+                                </div>
+                                <div className="p-4 w-auto">
+                                    <div>
+                                        {member.name ?? 'Unknown Contributor'}
+                                    </div>
+                                    <div className="text-neutral-400">
+                                        {member.title ?? 'Contributor'}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="mt-4">
+                    <div>Contributors 🤝</div>
+                    <div className="flex flex-wrap gap-2 mt-4">
+                        {general_contributors.map((member, index) => (
+                            <a
+                                href={member.url}
+                                className="w-12 h-12 aspect-square bg-neutral-300 relative overflow-hidden"
+                                key={index}
+                            >
                                 {member.avatar && (
                                     <img
                                         src={member.avatar}
-                                        alt=""
+                                        alt={member.name}
                                         className="w-full h-full object-cover grayscale"
                                     />
                                 )}
                                 {!member.avatar && (
-                                    <div className="text-6xl grayscale left-1/2 -translate-x-1/2 absolute bottom-0">
+                                    <div className="text-3xl grayscale left-1/2 -translate-x-1/2 absolute bottom-0">
                                         🤷‍♀️
                                     </div>
                                 )}
-                            </div>
-                            <div className="p-4 w-auto">
-                                <div>
-                                    {member.name ?? 'Unknown Contributor'}
-                                </div>
-                                <div className="text-neutral-400">
-                                    {member.title ?? 'Contributor'}
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                            </a>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
--- list.ts
+++ list.ts
@@ -2,6 +2,7 @@ type Person = {
     avatar?: string;
     name: string;
     title?: string;
+    url?: string;
 };
 
 export const list: Person[] = [
@@ -50,4 +51,80 @@ export const list: Person[] = [
     },
 ];
 
-export const external_contributor: Person[] = [];
+export const general_contributors: Person[] = [
+    {
+        name: 'Miguel Piedrafita',
+        avatar: '/contributor/miguel.jpeg',
+        url: 'https://miguel.build/',
+    },
+    {
+        name: 'Hattorius',
+        avatar: '/contributor/hatty.jpeg',
+        url: 'https://xlogic.sh/',
+    },
+    {
+        name: 'Joshua Hendrix',
+        avatar: '/contributor/josh.jpeg',
+        url: 'https://github.com/thejoshuahendrix',
+    },
+    {
+        name: 'Derock',
+        avatar: '/contributor/derock.jpeg',
+        url: 'https://derock.dev/',
+    },
+    {
+        name: 'Visen',
+        avatar: '/contributor/visen.jpeg',
+        url: 'https://visen.dev/',
+    },
+    {
+        name: 'Alan',
+        avatar: '/contributor/vlan.jpeg',
+        url: 'https://github.com/imvlan',
+    },
+    {
+        name: 'Samvel Petrosyan',
+        avatar: '/contributor/loidnoir.jpeg',
+        url: 'https://github.com/loidnoir',
+    },
+    {
+        name: 'Jonah "Jay" Y-M',
+        avatar: '/contributor/tgtechie.jpeg',
+        url: 'https://github.com/TG-Techie',
+    },
+    {
+        name: 'Michael Desormeaux',
+        avatar: '/contributor/michaeldesormeaux.jpeg',
+        url: 'https://home.mikedez.com/',
+    },
+    {
+        name: 'Carlos A',
+        avatar: '/contributor/chalkedgoodse.jpeg',
+        url: 'https://www.carlosalba.dev/',
+    },
+    {
+        name: 'Will (0xEquinox)',
+        avatar: '/contributor/equinox.png',
+        url: 'https://github.com/0xEquinox',
+    },
+    {
+        name: 'Kristóf Kékesi',
+        avatar: '/contributor/kristof.jpeg',
+        url: 'https://kekesi.dev',
+    },
+    {
+        name: 'MilkedCow',
+        avatar: '/contributor/milkedcow.png',
+        url: 'https://github.com/MilkedCow',
+    },
+    {
+        name: 'JoelImgu',
+        avatar: '/contributor/joelimgu.png',
+        url: 'joelimgu.github.io',
+    },
+    {
+        name: 'Oğuz Eray Orhan',
+        avatar: '/contributor/oguzeray.jpeg',
+        url: 'https://github.com/oguzeray',
+    },
+];
