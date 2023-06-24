4d7777aafebb0b86d0defc53fb0a49e00de5ba92
Luc <luc@lucemans.nl>
2023-06-24 05:17:25 +0000

    Introduce Image

--- app/[member]/page.tsx
+++ app/[member]/page.tsx
@@ -1,11 +1,18 @@
-import { general_contributors, list } from '../../list';
+import { list } from '../../list';
 
 export default function Page({ params }: { params: { member: string } }) {
+    const member = list.find((person) => person.slug === params.member);
+
+    if (!member) return <div>This member does not exist 🤷‍♀️</div>;
+
     return (
         <div className="mx-auto flex w-screen max-w-3xl items-center justify-center p-4 md:p-8">
             <div className="flex w-full flex-col gap-4">
                 <div className="whitespace-pre-wrap px-6 py-10">
                     &nbsp; &gt;&gt;&gt;&gt; This page is about {params.member}
+                </div>
+                <div>
+                    <img src={member.avatar} alt="" className="h-64 w-64" />
                 </div>
             </div>
         </div>
